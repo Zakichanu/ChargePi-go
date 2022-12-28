cd71f9e8e47d8749d3b11ea8c9a4ee84667cedbc
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2022-12-28 15:03:59 +0100

    Initial commit

---
+++ ui/src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let loading = false;
+
+	function handleSubmit() {
+		loading = true;
+
+		setTimeout(() => {
+			goto('/');
+		}, 500);
+	}
+</script>
+
+<div class="grid place-content-center h-3/4">
+	<h1 class="text-5xl font-bold mb-12 text-center text-white">ChargePI</h1>
+
+	<form
+		class="card w-96 bg-base-200 shadow-xl flex flex-col space-y-4 items-center p-10"
+		on:submit|preventDefault={handleSubmit}
+	>
+		<input
+			name="username"
+			type="text"
+			placeholder="Username"
+			class="input input-bordered w-full max-w-xs"
+			required
+		/>
+
+		<input
+			name="password"
+			type="password"
+			placeholder="Password"
+			class="input input-bordered w-full max-w-xs"
+			required
+		/>
+
+		<button type="submit" class="btn btn-primary w-2/3" class:loading>Login</button>
+	</form>
+</div>
---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../app.css';
+
+	export const ssr = false;
+
+	const ocppConnected = false;
+	const version = '1.0';
+</script>
+
+<div class="flex flex-col h-screen">
+	<div class="h-full">
+		<slot />
+	</div>
+
+	<footer class="footer items-center p-4 bg-neutral text-neutral-content">
+		<div class="items-center grid-flow-col">
+			<p>Copyright ChargePI © 2022 - All right reserved</p>
+		</div>
+		<div class="grid-flow-col gap-2 md:place-self-center md:justify-self-end">
+			<div
+				class="badge badge-success"
+				class:badge-success={ocppConnected}
+				class:badge-error={!ocppConnected}
+			>
+				OCPP {ocppConnected ? 'Connected' : 'Not Connected'}
+			</div>
+			<div class="badge badge-info">v{version}</div>
+		</div>
+	</footer>
+</div>
