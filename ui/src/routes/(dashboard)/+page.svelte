a877d38f50943423976454234b631956b94b83dd
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2022-12-28 16:28:34 +0100

    Added header

--- ui/src/routes/(dashboard)/+page.svelte
+++ ui/src/routes/(dashboard)/+page.svelte
@@ -1,36 +1,61 @@
-<div class="overflow-x-auto">
-	<table class="table w-full">
-		<!-- head -->
-		<thead>
-			<tr>
-				<th />
-				<th>Name</th>
-				<th>Job</th>
-				<th>Favorite Color</th>
-			</tr>
-		</thead>
-		<tbody>
-			<!-- row 1 -->
-			<tr>
-				<th>1</th>
-				<td>Cy Ganderton</td>
-				<td>Quality Control Specialist</td>
-				<td>Blue</td>
-			</tr>
-			<!-- row 2 -->
-			<tr>
-				<th>2</th>
-				<td>Hart Hagerty</td>
-				<td>Desktop Support Technician</td>
-				<td>Purple</td>
-			</tr>
-			<!-- row 3 -->
-			<tr>
-				<th>3</th>
-				<td>Brice Swyre</td>
-				<td>Tax Accountant</td>
-				<td>Red</td>
-			</tr>
-		</tbody>
-	</table>
+<div class="stats shadow">
+	<div class="stat">
+		<div class="stat-figure text-secondary">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-8 h-8 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+		</div>
+		<div class="stat-title">Downloads</div>
+		<div class="stat-value">31K</div>
+		<div class="stat-desc">Jan 1st - Feb 1st</div>
+	</div>
+
+	<div class="stat">
+		<div class="stat-figure text-secondary">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-8 h-8 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+				/></svg
+			>
+		</div>
+		<div class="stat-title">New Users</div>
+		<div class="stat-value">4,200</div>
+		<div class="stat-desc">↗︎ 400 (22%)</div>
+	</div>
+
+	<div class="stat">
+		<div class="stat-figure text-secondary">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-8 h-8 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+				/></svg
+			>
+		</div>
+		<div class="stat-title">New Registers</div>
+		<div class="stat-value">1,200</div>
+		<div class="stat-desc">↘︎ 90 (14%)</div>
+	</div>
 </div>
