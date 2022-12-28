92ec5d78dfd6997c524fe09883f8046c3f7f76ce
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2022-12-28 16:28:34 +0100

    Added header

--- ui/src/routes/(dashboard)/+layout.svelte
+++ ui/src/routes/(dashboard)/+layout.svelte
@@ -8,60 +8,76 @@
 		faCloud,
 		faFilePen,
 		faRightFromBracket,
+		faBars,
 	} from '@fortawesome/free-solid-svg-icons';
 	import NavItem from '$lib/navigation/NavItem.svelte';
+
+	let expand = true;
 </script>
 
-<div class="drawer drawer-end drawer-mobile h-full p-4">
-	<input id="side-menu" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content card bg-base-200 shadow-xl p-4">
-		<slot />
+<div class="flex flex-col h-full space-y-4 p-4">
+	<div class="navbar rounded-box bg-base-200 shadow-sm">
+		<div class="flex-1">
+			<span class="btn btn-ghost normal-case text-xl">ChargePI</span>
+		</div>
+		<div class="flex-none">
+			<button class="btn btn-square btn-ghost" on:click={() => (expand = !expand)}>
+				<Fa icon={faBars} />
+			</button>
+		</div>
 	</div>
-	<div class="drawer-side card bg-base-200 shadow-xl ml-4">
-		<label for="side-menu" class="drawer-overlay" />
-		<ul class="menu p-4 w-64 text-base-content">
-			<li>
-				<NavItem href="/">
-					<Fa icon={faHome} />
-					<span>Dashboard</span>
-				</NavItem>
-			</li>
-			<li>
-				<NavItem href="/evse">
-					<Fa icon={faBolt} />
-					<span>EVSE</span>
-				</NavItem>
-			</li>
-			<li>
-				<NavItem href="/card-reader">
-					<Fa icon={faCreditCard} />
-					<span>Card Reader</span>
-				</NavItem>
-			</li>
-			<li>
-				<NavItem href="/display">
-					<Fa icon={faDisplay} />
-					<span>Display</span>
-				</NavItem>
-			</li>
-			<li>
-				<NavItem href="/ocpp">
-					<Fa icon={faCloud} />
-					<span>OCPP</span>
-				</NavItem>
-			</li>
-			<li>
-				<NavItem href="/logs">
-					<Fa icon={faFilePen} />
-					<span>Logs</span>
-				</NavItem>
-			</li>
-			<li>
-				<NavItem href="/login">
-					<Fa icon={faRightFromBracket} />
-					<span>Logout</span>
-				</NavItem>
-			</li>
-		</ul>
+
+	<div class="flex h-full space-x-4">
+		<div class="card bg-base-200 shadow-sm grow p-4">
+			<slot />
+		</div>
+		{#if expand}
+			<div class="card bg-base-200 shadow-sm">
+				<ul class="menu w-64 p-4">
+					<li>
+						<NavItem href="/">
+							<Fa icon={faHome} />
+							Dashboard
+						</NavItem>
+					</li>
+					<li>
+						<NavItem href="/evse">
+							<Fa icon={faBolt} />
+							EVSE
+						</NavItem>
+					</li>
+					<li>
+						<NavItem href="/card-reader">
+							<Fa icon={faCreditCard} />
+							Card Reader
+						</NavItem>
+					</li>
+					<li>
+						<NavItem href="/display">
+							<Fa icon={faDisplay} />
+							Display
+						</NavItem>
+					</li>
+					<li>
+						<NavItem href="/ocpp">
+							<Fa icon={faCloud} />
+							OCPP
+						</NavItem>
+					</li>
+					<li>
+						<NavItem href="/logs">
+							<Fa icon={faFilePen} />
+							Logs
+						</NavItem>
+					</li>
+					<li>
+						<NavItem href="/login">
+							<Fa icon={faRightFromBracket} />
+							Logout
+						</NavItem>
+					</li>
+				</ul>
+			</div>
+		{/if}
 	</div>
 </div>
